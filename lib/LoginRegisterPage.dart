63efa6b6f9f6459f861f31dde15a528c0befa05c
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-04-18 22:20:22 +0530

    Create Link and some error fixed

--- lib/LoginRegisterPage.dart
+++ lib/LoginRegisterPage.dart
@@ -5,14 +5,34 @@ class LoginRegisterPage extends StatefulWidget {
   _LoginRegisterPageState createState() => _LoginRegisterPageState();
 }
 
+enum FormType{
+  login,
+  register
+}
+
 class _LoginRegisterPageState extends State<LoginRegisterPage> {
   
+  final formKey = GlobalKey<FormState>();
+  FormType _formType = FormType.login;
+  String _email = "";
+  String _password = "";
+
   void validateAndSave(){
 
   }
 
   void moveToRegister(){
+    formKey.currentState.reset();
+    setState(() {
+      _formType =  FormType.register;
+    });
+  }
 
+  void moveToLogin(){
+    formKey.currentState.reset();
+    setState(() {
+      _formType =  FormType.login;
+    });
   }
   
   @override
@@ -22,56 +42,103 @@ class _LoginRegisterPageState extends State<LoginRegisterPage> {
         title: Center(child: Text("My Blog App")),
         backgroundColor: Colors.black,
       ),
+
       body: Container(
         margin: EdgeInsets.all(15.0),
         child: Form(
+          key: formKey,
+
           child:Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              SizedBox(height: 10.0),
-              Hero(
-                tag: 'hero',
-                  child: CircleAvatar(
-                    backgroundColor: Colors.transparent,
-                    radius: 55.0,
-                    child: Image.asset('images/homelogo.png'),
-                  ),
-                ),
-              SizedBox(height: 20.0),
-
-              //Email
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Email'
-                ),
-              ),
-              SizedBox(height: 10.0),
-              //Password
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Password'
-                ),
-              ),
-              SizedBox(height: 20.0),
-
-              //LoginButton
-              RaisedButton(
-                child: Text("Login",style: TextStyle(fontSize: 20.0)),
-                textColor: Colors.white,
-                color: Colors.pink,
-                onPressed: validateAndSave,
-              ),
-
-              //CreateButton
-              FlatButton(
-                child: Text("Not have a Account? Create Account!",style: TextStyle(fontSize: 14.0)),
-                textColor: Colors.red,
-                onPressed: moveToRegister,
-              ),
-            ],
+            children: createInputs() + createButtons(),
           ),
         ),
       ),
     );
   }
+
+
+  List<Widget> createInputs(){
+    return[
+      SizedBox(height: 10.0),
+      logo(),
+      SizedBox(height: 20.0),
+
+      //Email
+      TextFormField(
+        decoration: InputDecoration(
+          labelText: 'Email'
+        ),
+        validator: (value){
+          return value.isEmpty ? 'Email is required' : null;
+        },
+        onSaved: (value){
+          return _email = value;
+        },
+      ),
+      SizedBox(height: 10.0),
+
+      //Password
+      TextFormField(
+        decoration: InputDecoration(
+          labelText: 'Password'
+        ),
+      ),
+      SizedBox(height: 20.0),
+    ];
+  }
+
+  Widget logo(){
+    return
+      Hero(
+        tag: 'hero',
+        child: CircleAvatar(
+          backgroundColor: Colors.transparent,
+          radius: 55.0,
+          child: Image.asset('images/homelogo.png'),
+        ),
+      );   
+  }
+
+  List<Widget> createButtons(){
+    if(_formType == FormType.login){
+        return[
+          //LoginButton
+          RaisedButton(
+            child: Text("Login", style: TextStyle(fontSize: 20.0)),
+            textColor: Colors.white,
+            color: Colors.pink,
+            onPressed: validateAndSave,
+          ),
+
+          //CreateButton
+          FlatButton(
+            child: Text("Not have a Account? Create Account!", style: TextStyle(fontSize: 14.0)),
+            textColor: Colors.red,
+            onPressed: moveToRegister,
+          ),
+        ];
+      }
+    else{
+      return[
+          //LoginButton
+
+          RaisedButton(
+            child: Text("Create", style: TextStyle(fontSize: 20.0)),
+            textColor: Colors.white,
+            color: Colors.pink,
+            onPressed: validateAndSave,
+          ),
+
+          //CreateButton
+          FlatButton(
+            child: Text("Already have a Account? Login!", style: TextStyle(fontSize: 14.0)),
+            textColor: Colors.red,
+            onPressed: moveToLogin,
+          ),
+        ];
+      }
+  }
+
 }
+
