da115ed155bb17931f365762ee36b49c84ed0500
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-04-18 18:31:31 +0530

    Add login Page

---
+++ lib/LoginRegisterPage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LoginRegisterPage extends StatefulWidget {
+  @override
+  _LoginRegisterPageState createState() => _LoginRegisterPageState();
+}
+
+class _LoginRegisterPageState extends State<LoginRegisterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My Blog App"),
+      ),
+    );
+  }
+}
